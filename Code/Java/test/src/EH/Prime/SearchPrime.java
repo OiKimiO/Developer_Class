904bae1f5c065ed955e9e440c8b7e60f43aaf95e
Eun Ho Kim <etkim02@naver.com>
2021-06-21 19:41:08 +0900

    다시 보자

--- Code/Java/test/src/EH/Prime/SearchPrime.java
+++ Code/Java/test/src/EH/Prime/SearchPrime.java
@@ -1,10 +1,10 @@
+package EH.Prime;
+
 public class SearchPrime {
 //11
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	  System.out.println("확인중");
-	  // utf-8임
-	  // 와우
+	  
 	}
 
 }
