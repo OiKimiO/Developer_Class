b6e9b7294f3190f6a0fd6bb155e8f0653179797a
Eun Ho Kim <etkim02@naver.com>
2021-06-22 11:59:39 +0900

    Update SearchPrime.java

--- Code/Java/test/src/EH/Prime/SearchPrime.java
+++ Code/Java/test/src/EH/Prime/SearchPrime.java
@@ -3,7 +3,7 @@ package EH.Prime;
 public class SearchPrime {
 //11
 	public static void main(String[] args) {
-		System.out.println("브랜치 잘 적용되는지 확인11112222");
+		System.out.println("브랜치 잘 적용되는지 확인1111222244444");
 	}
 
 }
